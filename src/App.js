2981b5f7ed6832007f48fb0ccaaf3abc89679e48
ElenaBartoshuk <elena.vivatravel@gmail.com>
2022-09-06 22:24:28 +0300

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import Dictionary from "./Dictionary/Dictionary.js";
+
+import Footer from "./Footer/Footer.js";
+import "./styles.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="App_content">
+          <main>
+            <Dictionary />
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Dictionary/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../Dictionary/Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {}
+
+  function Search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input type="Search" autoFocus={true} onChange={handleKeywordChange} />
+        <button type=""></button>
+      </form>
+    </div>
+  );
+}
